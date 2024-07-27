27ebb185594f2eb75cb0852a470ea849d13060a8
theapache64 <theapache64@gmail.com>
2024-07-27 14:18:12 +0530

    👕 var to let

--- tlog.js
+++ tlog.js
@@ -1,21 +1,21 @@
 // get current date in format dd/mm/yyyy
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0');
-var yyyy = today.getFullYear();
+let today = new Date();
+const dd = String(today.getDate()).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, '0');
+const yyyy = today.getFullYear();
 today = dd + '/' + mm + '/' + yyyy;
 console.log("Today: " + today);
-var url = "https://docs.google.com/spreadsheets/d/1pvwjyRWvRPzHoacraKi4w6324-BErl3Bs-Q9HIwbeGI/gviz/tq?tqx=out:csv&sheet=Sheet1&tq=SELECT B WHERE D = '"+ today +"'"
+const url = "https://docs.google.com/spreadsheets/d/1pvwjyRWvRPzHoacraKi4w6324-BErl3Bs-Q9HIwbeGI/gviz/tq?tqx=out:csv&sheet=Sheet1&tq=SELECT B WHERE D = '" + today + "'";
 console.log("URL: " + url);
 
 // Get blog URL from Google Sheet
-var xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 xhr.open("GET", url , true);
 xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
         let responseText = xhr.responseText;
         console.log("Response: " + responseText);
-        var blogUrl = responseText.split("\n")[1];
+        let blogUrl = responseText.split("\n")[1];
         console.log("URL: " + blogUrl);
 
         // remove `"` start and end of the URL
